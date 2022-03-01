358f4ad62b5c5a28e2172af891a64337b7f694b6
eli.zwy <wenyang.zhang@ipinyou.com>
2022-03-01 21:18:38 +0800

    state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import NameCard from './components/NameCard.js';
+import NameCard from './components/NameCard';
+import LikesButton from './components/LikesButton';
 
 function App() {
   const tags=['aaa','bbb','ccc']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
       <NameCard name="zwy" number={12345678} isHuman tags={tags}/>
+      <LikesButton />
     </div>
   );
 }
